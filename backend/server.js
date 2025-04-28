c3b1a2fc4ffe624ae9bb804803bfe1266148dc33
c137v8 <147223475+c137v8@users.noreply.github.com>
2025-04-28 08:09:26 +0530

    it hot

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/auth');
 
 // Load environment variables
 dotenv.config();
